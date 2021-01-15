e59bf800a1379eaeddb877f624086ba3fca6288f
Wenjing <wenjing.liu.wang@gmail.com>
2021-01-15 18:40:36 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,97 @@
-import React,  { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// Router is the process of resource navigation in a web app
-class App extends React.Component {
-  render() {
-    return (
-      <div class="container">
-        <h1 class="child">Welcome!</h1>
-        <img src="react.png" width="800" height="500" alt="React"></img>
-      </div>
-    )
-  }
+import Home from "./components/Home";
+import About from "./components/About";
+import Notfound from "./components/Notfound";
 
-}
+import Products from "./components/Products";  
+import ProductItem from "./components/ProductItem"; 
+import Information from "./components/Information";  
+import Users from "./components/Users";
+import Clients from "./components/Clients";
+import Contact from "./components/Contact";
+
+import Signup from "./components/Signup";
+import Login from "./components/Login";  
+import TextEditor from "./components/TextEditor";  
+
+import { useTranslation } from "react-i18next";
+import i18next from 'i18next';
+
+function App() { 
+     const { t } = useTranslation(); 
+
+     function handleChange(lang) {
+          i18next.changeLanguage(lang);
+     }
+
+     return (
+          <div className="container-fluid">
+               <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <Link to={"/home"} className="navbar-brand">React</Link>
+                    <div className="navbar-nav mr-auto"> 
+                         <li className="nav-item">
+                              <Link to={"/products"} className="nav-link">{t("Products")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/clients"} className="nav-link">{t("Clients")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/users"} className="nav-link">{t("Users")}</Link>
+                         </li>
+
+                         <li className="nav-item">
+                              <Link to={"/information"} className="nav-link">{t("Information")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/contact"} className="nav-link">{t("Contact")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/about"} className="nav-link">{t("About")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/text-editor"} className="nav-link">{t("Blog")}</Link>
+                         </li>
+                    </div>
+
+                    <div className="navbar-nav mr-1">
+                         <button onClick={()=>handleChange('en')}>En</button>
+                         <button onClick={()=>handleChange('ch')}>Ch</button>
+
+                         <li className="nav-item">
+                              <Link to={"/signup"} className="nav-link">{t("Signup")}</Link>
+                         </li>
+                         <li className="nav-item">
+                              <Link to={"/login"} className="nav-link">{t("Login")}</Link>
+                         </li>
+                    </div>
+               </nav>
+
+               <div className="container">
+                    <Switch>
+                         <Route exact path="/home" component={Home} />
+                         <Route path="/products" exact component={Products} /> 
+                         <Route path="/products/:id" component={ProductItem} /> 
+                         <Route path="/clients" component={Clients} />
+                         <Route path="/users" component={Users} />
+
+                         <Route path="/information" component={Information} />
+                         <Route path="/contact" component={Contact} />
+                         <Route path="/about" component={About} />
+                         <Route path="/text-editor" component={TextEditor} />
+
+                         <Route path="/signup" component={Signup} />
+                         <Route path="/login" component={Login} />
+                         <Route component={Notfound} />
+                    </Switch>
+               </div>
+          
+        </div>
+     )  
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,76 +1,51 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
-import { Route, NavLink, BrowserRouter as Router, Switch } from "react-router-dom";
-import App from './App'; 
-import Users from "./components/Users";
-import Clients from "./components/Clients";
-import Products from "./components/Products";  
-import ProductItem from "./components/ProductItem";    
-import Information from "./components/Information";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Notfound from "./components/Notfound";
-import Signup from "./components/Signup";
-import Login from "./components/Login";  
+import i18next from "i18next";
+import { I18nextProvider } from 'react-i18next';  
+import LanguageDetector from 'i18next-browser-languagedetector'; 
 
-const routing = (
-	<Router>
-		<div>
-			<div className="row">
-                <div className="col-md-12">
-                    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                        <Navbar.Brand href="#">LOGO</Navbar.Brand>
+import translationEn from "./locales/en/common.json"; 
+import translationCh from "./locales/ch/common.json";
 
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/users">Users</Nav.Link>
-                                <Nav.Link href="/clients">Clients</Nav.Link>
-                                <Nav.Link href="/products">Products</Nav.Link>
-                                <Nav.Link href="/information">Information</Nav.Link>
-                                <Nav.Link href="/about">About Us</Nav.Link>
-                                <Nav.Link href="/contact">Contact Us</Nav.Link>
-                            </Nav>
-                                    
-                            <Form inline>
-                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
+const resources = {
+  en: {
+    translation: translationEn
+  },
+  ch: {
+    translation: translationCh
+  }
+};
 
-                            <Nav className="mr-1">
-                                <Nav.Link href="/signup">Signup</Nav.Link>
-                                <Nav.Link href="/login">Login</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
+i18next
+	.use(LanguageDetector)
+	.init({
+    resources, 
 
-					<Switch>
-						<Route exact path="/" component={App} />
-						<Route path="/users" component={Users} /> 
-						<Route path="/clients" component={Clients} /> 
-                        <Route path="/products" exact component={Products} /> 
-                        <Route path="/products/:id" component={ProductItem} /> 
+    lng: 'en',
+    fallbackLng: 'en', // use 'en' if detected lng is not available
+    debug: true,
+    preload: navigator.languages,
+    keySeparator: false, // use flat json
 
-						<Route path="/information" component={Information} /> 
-						<Route path="/about" component={About} /> 
-						<Route path="/contact" component={Contact} />
-                        <Route path="/signup" component={Signup} /> 
-                        <Route path="/login" component={Login} />  
-						<Route component={Notfound} />
-					</Switch>
-				</div>
-			</div>
-		</div>
-	</Router>
-);
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+});
 
+/*
+	To make the internationalization functions visible 
+	in all our components, we have to wrap the App component 
+	with I18nextProvider/IntlProvider.
+*/
 ReactDOM.render(
-  routing,
+  <BrowserRouter>
+  	<I18nextProvider i18n={i18next}>
+  		<App />
+	</I18nextProvider> 
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-
+ 
